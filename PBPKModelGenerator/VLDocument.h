9d01519242c02ff6cbde950ee8a776934c7e9823
jeffreyvarner <jeff@curveos.com>
2013-10-02 18:57:59 -0400

    Initial Commit

---
+++ PBPKModelGenerator/VLDocument.h
@@ -0,0 +1,13 @@
+//
+//  VLDocument.h
+//  PBPKModelGenerator
+//
+//  Created by Jeffrey Varner on 10/2/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLDocument : NSDocument
+
+@end
---
+++ PBPKModelGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PBPKModelGenerator
+//
+//  Created by Jeffrey Varner on 10/2/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
